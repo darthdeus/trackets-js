6ca8c65293041074dcd508cdab82d1267ebbfd0a
Jakub Arnold <darthdeus@gmail.com>
2014-01-20 17:33:43 +0100

    Remove gulpfile.js since we no use it no more

--- gulpfile.js
+++
@@ -1,9 +0,0 @@
-var gulp = require('gulp');
-var closureCompiler = require('gulp-closure-compiler');
-
-gulp.task('default', function(){
-  // place code for your default task here
-  gulp.src('src/main.js')
-    .pipe(closureCompiler())
-    .pipe(gulp.dest('dist'));
-});
