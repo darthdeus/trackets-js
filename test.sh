f6edf0a8efb1c29c74c2b0e7d344cdd501c3bae5
Jakub Arnold <darthdeus@gmail.com>
2014-03-09 00:42:36 +0100

    Cleanup test.sh

--- test.sh
+++ test.sh
@@ -4,10 +4,12 @@ set -e
 echo "Starting web server"
 asdf 1>&2 2> /dev/null &
 echo "Waiting for the server to start up"
-sleep 2
+
 SERVER=$!
+
+sleep 2
 
 echo "Running phantom JS"
 phantomjs runner.js http://localhost:9292/qunit.html
 
-kill $SERVER
+kill -15 $SERVER
