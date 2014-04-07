482ac7723eec4f2bf9a1c597f3e8c80095c43aec
Jakub Arnold <darthdeus@gmail.com>
2014-04-07 13:13:56 +0200

    Fix whitespace

--- src/jquery.js
+++ src/jquery.js
@@ -2,16 +2,20 @@ goog.require("trackets.wrapper");
 goog.provide("trackets.jquery");
 
 var isJqueryInit = false;
+
 function initJquery(errorHandler) {
-  if(isJqueryInit) return true;
-  if(!window.jQuery || !window.jQuery.fn) return false;
+  if (isJqueryInit) return true;
+  if (!window.jQuery || !window.jQuery.fn) return false;
 
   var _jQueryReady = jQuery.fn.ready;
-  if(_jQueryReady) {
+
+  if (_jQueryReady) {
     isJqueryInit = true;
+    
     jQuery.fn.ready = function(f) {
       return _jQueryReady(wrap(f, errorHandler));
     };
+
     return true;
   }
 
