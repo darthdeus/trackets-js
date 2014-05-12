0c9715fbffcad0689b99e22ee7e2a55d922e30dd
Jan Votava <votava@deployment.cz>
2014-05-13 00:52:46 +0200

    Use full polyfill XMLHttpRequest function

--- src/ajax.js
+++ src/ajax.js
@@ -1,7 +1,7 @@
 goog.provide("trackets.ajax");
 
 function sendRequest(url, postData) {
-  var req = createXMLHTTPObject();
+  var req = window.XMLHttpRequest();
   if (!req) return;
   if(postData.length > 100000) return;
 
@@ -17,40 +17,4 @@ function sendRequest(url, postData) {
       }
     }
   }
-}
-
-var XMLHttpFactories = [
-  function () {return new XMLHttpRequest(); },
-  function () {return new ActiveXObject("Msxml2.XMLHTTP"); },
-  function () {return new ActiveXObject("Msxml3.XMLHTTP"); },
-  function () {return new ActiveXObject("Microsoft.XMLHTTP"); }
-];
-
-function createXMLHTTPObject() {
-  var xmlhttp = false;
-  for (var i= 0; i < XMLHttpFactories.length; i++) {
-    try {
-      xmlhttp = XMLHttpFactories[i]();
-
-      xmlhttp.UNSENT = 0;
-      xmlhttp.OPENED = 1;
-      xmlhttp.HEADERS_RECEIVED = 2;
-      xmlhttp.LOADING = 3;
-      xmlhttp.DONE = 4;
-
-      if(i > 1) {
-        XMLHttpRequest.prototype.send = function(body) {
-          if (body instanceof FormData) {
-            this.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-            arguments[0] = body.toString();
-          }
-          return send.apply(this, arguments);
-        }
-      }
-    } catch (e) {
-      continue;
-    }
-    break;
-  }
-  return xmlhttp;
 }
--- src/polyfill.js
+++ src/polyfill.js
@@ -198,3 +198,58 @@ window.removeEvent = function (obj, type, fn) {
     obj["e" + type + fn] = null;
   }
 };
+
+//
+// XMLHttpRequest (http://www.w3.org/TR/XMLHttpRequest/)
+//
+var XMLHttpRequest = window.XMLHttpRequest || function () {
+  /*global ActiveXObject*/
+  try { return new ActiveXObject("Msxml2.XMLHTTP.6.0"); } catch (e1) { }
+  try { return new ActiveXObject("Msxml2.XMLHTTP.3.0"); } catch (e2) { }
+  try { return new ActiveXObject("Msxml2.XMLHTTP"); } catch (e3) { }
+  throw Error("This browser does not support XMLHttpRequest.");
+};
+
+XMLHttpRequest.UNSENT = 0;
+XMLHttpRequest.OPENED = 1;
+XMLHttpRequest.HEADERS_RECEIVED = 2;
+XMLHttpRequest.LOADING = 3;
+XMLHttpRequest.DONE = 4;
+
+window.XMLHttpRequest = XMLHttpRequest;
+
+//
+// FormData (http://www.w3.org/TR/XMLHttpRequest2/#interface-formdata)
+//
+if (!('FormData' in window)) {
+  (function(global) {
+    function FormData(form) {
+      this._data = [];
+      if (!form) return;
+      for (var i = 0; i < form.elements.length; ++i)
+        this.append(form.elements[i].name, form.elements[i].value);
+    }
+
+    FormData.prototype.append = function(name, value /*, filename */) {
+      if ('Blob' in global && value instanceof global.Blob) throw TypeError("Blob not supported");
+      name = String(name);
+      this._data.push([name, value]);
+    };
+
+    FormData.prototype.toString = function() {
+      return this._data.map(function(pair) {
+        return encodeURIComponent(pair[0]) + '=' + encodeURIComponent(pair[1]);
+      }).join('&');
+    };
+
+    global.FormData = FormData;
+    var send = global.XMLHttpRequest.prototype.send;
+    global.XMLHttpRequest.prototype.send = function(body) {
+      if (body instanceof FormData) {
+        this.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+        arguments[0] = body.toString();
+      }
+      return send.apply(this, arguments);
+    };
+  }(this));
+}
