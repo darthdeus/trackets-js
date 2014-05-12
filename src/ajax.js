45ba5079a918e7552ddbf9b8c3cbb14b0555ad4f
Jan Votava <votava@deployment.cz>
2014-05-13 00:13:46 +0200

    XMLHttpRequest: better send prototype to fix Access Denied

--- src/ajax.js
+++ src/ajax.js
@@ -38,41 +38,13 @@ function createXMLHTTPObject() {
       xmlhttp.LOADING = 3;
       xmlhttp.DONE = 4;
 
-      //
-      // FormData (http://www.w3.org/TR/XMLHttpRequest2/#interface-formdata)
-      //
-      if (!('FormData' in window)) {
-        (function(global) {
-          function FormData(form) {
-            this._data = [];
-            if (!form) return;
-            for (var i = 0; i < form.elements.length; ++i)
-              this.append(form.elements[i].name, form.elements[i].value);
-          }
-
-          FormData.prototype.append = function(name, value /*, filename */) {
-            if ('Blob' in global && value instanceof global.Blob) throw TypeError("Blob not supported");
-            name = String(name);
-            this._data.push([name, value]);
-          };
-
-          FormData.prototype.toString = function() {
-            return this._data.map(function(pair) {
-              return encodeURIComponent(pair[0]) + '=' + encodeURIComponent(pair[1]);
-            }).join('&');
-          };
-
-          global.FormData = FormData;
-          var send = global.XMLHttpRequest.prototype.send;
-          global.XMLHttpRequest.prototype.send = function(body) {
-            if (body instanceof FormData) {
-              this.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-              arguments[0] = body.toString();
-            }
-            return send.apply(this, arguments);
-          };
-        }(this));
-      }
+      XMLHttpRequest.prototype.send = function(body) {
+        if (body instanceof FormData) {
+          this.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+          arguments[0] = body.toString();
+        }
+        return send.apply(this, arguments);
+      };
     } catch (e) {
       continue;
     }
